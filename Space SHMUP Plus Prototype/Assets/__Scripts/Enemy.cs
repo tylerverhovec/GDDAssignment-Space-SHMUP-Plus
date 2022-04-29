3d991e44df33161edd43cd730bf53932f015ced6
Tyler V <verhovectyler@gmail.com>
2022-04-28 19:00:36 -0600

    Enemies react to damage taken

--- Space SHMUP Plus Prototype/Assets/__Scripts/Enemy.cs
+++ Space SHMUP Plus Prototype/Assets/__Scripts/Enemy.cs
@@ -8,11 +8,25 @@ public class Enemy : MonoBehaviour {
     public float      fireRate = 0.3f;  // Seconds/shot (Unused)
     public float      health = 10;
     public int        score = 100;      // Points earned for destroying this
+    public float      showDamageDuration = 0.1f;
+
+    [Header("Set Dynamically: Enemy")]
+    public Color[]    originalColors;
+    public Material[] materials;// All the Materials of this & its children
+    public bool       showingDamage = false;
+    public float      damageDoneTime; // Time to stop showing damage
+    public bool       notifiedOfDestruction = false; // Will be used later
 
     protected BoundsCheck bndCheck;
 
     void Awake() {
         bndCheck = GetComponent<BoundsCheck>();
+        // Get materials and colors for this GameObject and its children
+        materials = Utils.GetAllMaterials( gameObject );
+        originalColors = new Color[materials.Length];
+        for (int i=0; i<materials.Length; i++) {
+            originalColors[i] = materials[i].color;
+        }
     }
 
     // This is a Property: A method that acts like a field
@@ -27,6 +41,10 @@ public class Enemy : MonoBehaviour {
 
     void Update() {
         Move();
+
+        if ( showingDamage && Time.time > damageDoneTime ) {
+            UnShowDamage();
+        }
 
         if ( bndCheck != null && bndCheck.offDown) {
             // We're off the bottom, so destroy this GameObject
@@ -52,6 +70,7 @@ public class Enemy : MonoBehaviour {
                 }
 
                 // Hurt this Enemy
+                ShowDamage();
                 // Get the damage amount from the Main WEAP_DICT.
                 health -= Main.GetWeaponDefinition(p.type).damageOnHit;
                 if (health <= 0) {
@@ -66,6 +85,21 @@ public class Enemy : MonoBehaviour {
                 break;
 
         }
+    }
+
+    void ShowDamage() {
+        foreach (Material m in materials) {
+            m.color = Color.red;
+        }
+        showingDamage = true;
+        damageDoneTime = Time.time + showDamageDuration;
+    }
+
+    void UnShowDamage() {
+        for ( int i=0; i<materials.Length; i++ ) {
+            materials[i].color = originalColors[i];
+        }
+        showingDamage = false;
     }
 
 }
